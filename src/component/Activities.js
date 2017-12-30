a2b213ba272f9a4b6bfc582b4518dd290a063b29
Sasha Ponomarov <randirsbox@gmail.com>
2017-12-30 13:08:21 +0200

    Remove, increment, derement activity, Start adding firebase

--- src/component/Activities.js
+++ src/component/Activities.js
@@ -1,32 +1,47 @@
 import './Activities.css';
+import Activity from './Activity';
 import React, { Component } from 'react';
 import Paper from 'material-ui/Paper';
-
+import {List} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
 
 class Activities extends Component {
-
-  sortByTime(arr) {
-    return arr.sort((a, b) => b.time - a.time);
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false
+    }
+  }
+  expand = (e) => {
+    this.setState({expanded: +e.currentTarget.id});
   }
 
   render() {
-    const types = this.props.types;
+    const {types, removeActivity, changeAmount} = this.props;
     let prevDate = '';
     let activities = [];
-    let activitiesData = this.sortByTime(this.props.activities);
+    let activitiesData = this.props.sortByTime(this.props.activities);
     activitiesData.forEach((activity, i) => {
       let time = new Date(activity.time);
       if (time.toDateString() !== prevDate) {
         prevDate = time.toDateString();
-        activities.push(<span key={"date"+i}>{prevDate}</span>);
+        activities.push(<Subheader key={"date"+i}>{prevDate}</Subheader>);
       }
-      activities.push(<li key={i}>{activity.count} {types[activity.type].name} {time.getHours()}:{time.getMinutes()}</li>);
+      let activityProps = {
+        key: i,
+        id: i, 
+        expanded: i===this.state.expanded,
+        types, activity, time, 
+        expand: this.expand,
+        removeActivity, changeAmount
+      }
+      activities.push(<Activity {...activityProps} />);
     })
     return (
       <Paper className="Activities"> 
-        <ul>
+        <List>
           {activities}
-        </ul>
+        </List>
       </Paper>
     );
   }
--- src/component/App.js
+++ src/component/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import firebase from '../config/firebase';
+
 import './App.css';
 import Activities from './Activities';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -13,15 +15,47 @@ class App extends Component {
         {name: 'Pull-ups'},
         {name: 'Sit-ups'}
       ],
-      activities: [{count: 10, type: 0, time: 1513590761061}, {count: 15, type: 0, time: 1513690761061}, 
-      {count: 15, type: 0, time: 1513710761061}, {count: 1, type: 1, time: 1513790761061}, {count: 1, type: 1, time: 1512690761061}]
+      // activities: [{count: 10, type: 0, time: 1513590761061}, {count: 15, type: 0, time: 1513690761061}, 
+      // {count: 15, type: 0, time: 1513710761061}, {count: 1, type: 1, time: 1513790761061}, {count: 3, type: 1, time: 1512690761061}]
+      activities: []
+    };
+    this.timeLimit = 5000;
+  }
+
+  componentDidMount() {
+    const activitiesRef = firebase.database().ref('activities');
+    activitiesRef.on('value', snapshot => {
+      this.setState({activities: Object.values(snapshot.val())})
+    })
+  }
+
+  addActivity = (e) => {
+    const activitiesRef = firebase.database().ref('activities');
+
+    let time = new Date();
+    let type = +e.currentTarget.id;
+    let activities = this.sortByTime([...this.state.activities]);
+    if (Math.abs(time - activities[0].time) < this.timeLimit && type === activities[0].type) {
+      activities[0].count += 10;
+      this.setState({activities});
+    } else {
+      let newActivity = {count: 10, type: type, time: time.getTime()};
+      activitiesRef.push(newActivity);
+      // this.setState({activities: [...activities, newActivity]});
     }
   }
-  addActivity = (e) => {
-    let time = new Date();
-    let newActivity = {count: 10, type: e.currentTarget.id, time: time.getTime()};
-    this.setState({activities: [...this.state.activities, newActivity]});
-    console.log(this.state)
+  removeActivity = (id) => {
+    this.setState({activities: this.state.activities.filter((a, i) => i !== id )});
+  }
+  changeAmount = (id, sign, amount) => {
+    let newActivities = [...this.state.activities];
+    if (sign === "minus") {amount = -amount}
+    newActivities[id].count += +amount;
+    if (newActivities[id].count < 0) {newActivities[id].count = 0}
+    this.setState({activities: newActivities});
+  }
+  sortByTime(arr) {
+    return arr.sort((a, b) => b.time - a.time);
   }
   render() {
     return (
@@ -33,7 +67,8 @@ class App extends Component {
           <main>
             <RaisedButton id="0" label="Pull-ups" className="AddActivity" onClick={this.addActivity} />
             <RaisedButton id="1" label="Sit-ups" className="AddActivity" onClick={this.addActivity} />
-            <Activities activities={this.state.activities} types={this.state.types} />
+            <Activities activities={this.state.activities} types={this.state.types} 
+                        sortByTime={this.sortByTime} removeActivity={this.removeActivity} changeAmount={this.changeAmount} />
           </main>
         </div>
       </MuiThemeProvider>
