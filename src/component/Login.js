73255a2e119a0ecc2e5cc6fc63d360906576ec06
Sasha Ponomarov <randirsbox@gmail.com>
2018-03-05 00:03:52 +0200

    Makes appbar look better on mobile devices

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 import './Login.css';
+import Paper from 'material-ui/Paper';
 import ListItem from 'material-ui/List/ListItem';
 import Avatar from 'material-ui/Avatar';
 import FlatButton from 'material-ui/FlatButton';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showProfile: false
+    }
+  }
+
+  toggleProfile = () => {
+    this.setState({showProfile: !this.state.showProfile})
+  }
 
 
   render() {
     const {user, handleLogin, handleLogout} = this.props
     let loginButton = user ?
-       [<ListItem 
+       <ListItem 
           key="0"
-          disabled={true}
-          leftAvatar={<Avatar src={user.photoURL} />}
+          // disabled={true}
+          onClick={this.toggleProfile}
+          rightAvatar={<Avatar src={user.photoURL} />}
         >
-          {user.displayName}
-        </ListItem>,
-        <FlatButton key="1" label="Log out" onClick={handleLogout} />]
+          <div className="username inverted-palette">{user.displayName}</div>
+        </ListItem>
       :
-        <FlatButton label="Log in" onClick={handleLogin} />
+        <FlatButton className="inverted-palette" label="Log in" onClick={handleLogin} />
 
     return (
       <div className="Login">
         {loginButton}
+        {this.state.showProfile && 
+        <Paper className="Profile">
+          <FlatButton key="1" label="Log out" onClick={() => {handleLogout(); this.toggleProfile()}} />
+        </Paper>}
       </div>
     );
   }
